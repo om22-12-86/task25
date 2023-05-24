653ecc4141b8170be3bdef4726377b22f6343248
om <130356845+om22-12-86@users.noreply.github.com>
2023-05-24 14:50:08 +0300

    25

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+// int[] RandomArray()
+// {
+//     int[] Arr = new int[186];
+
+//     for (int i = 0; i < Arr.Length; i++)
+//     {
+//         Arr[i] = new Random().Next(0,2);
+//     }
+//     return Arr;
+// }
+
+// int[] Arr = RandomArray();
+// for (int i = 0; i < Arr.Length; i++)
+// {
+//     System.Console.WriteLine(Arr[i]);
+// }
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+    Console.WriteLine("Введите целое число А: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите целое число B: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int x = a;
+    for (int i = 2; i <= b; i++)
+    {
+      if (b == 0)
+      { x = 1; }
+      else if(b == 1)
+      { x = a; }
+      else
+      {x = x * a;}
+    }
+    Console.WriteLine(x);
