aade63412007e0ae1c629928ee5d83d972c3b808
om <130356845+om22-12-86@users.noreply.github.com>
2023-05-24 16:22:58 +0300

    27

--- Program.cs
+++ Program.cs
@@ -25,18 +25,43 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-    Console.WriteLine("Введите целое число А: ");
-    int a = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите целое число B: ");
-    int b = Convert.ToInt32(Console.ReadLine());
-    int x = a;
-    for (int i = 2; i <= b; i++)
+    // Console.WriteLine("Введите целое число А: ");
+    // int a = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите целое число B: ");
+    // int b = Convert.ToInt32(Console.ReadLine());
+    // int x = a;
+    // for (int i = 2; i <= b; i++)
+    // {
+    //   if (b == 0)
+    //   { x = 1; }
+    //   else if(b == 1)
+    //   { x = a; }
+    //   else
+    //   {x = x * a;}
+    // }
+    // Console.WriteLine(x);
+
+//     Задача 27: Напишите программу,
+//      которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите целое число A: ");
+   string A = Console.ReadLine();
+   int x = 0;
+    
+   for (int i = 0; i < A.Length; i++)
     {
-      if (b == 0)
-      { x = 1; }
-      else if(b == 1)
-      { x = a; }
+      //Console.WriteLine(Int32.Parse(Convert.ToString(A[i])));
+      if (A.Length == 1)
+      {
+       x = Int32.Parse(Convert.ToString(A[i]));
+      }
       else
-      {x = x * a;}
-    }
+      {
+     
+      x = x + Int32.Parse(Convert.ToString(A[i]));
+      }
+    } 
     Console.WriteLine(x);
