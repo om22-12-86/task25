ec821afba0a063b2115491d00003fbeb9618a4c9
om <130356845+om22-12-86@users.noreply.github.com>
2023-05-24 17:03:37 +0300

    29

--- Program.cs
+++ Program.cs
@@ -47,21 +47,33 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.WriteLine("Введите целое число A: ");
-   string A = Console.ReadLine();
-   int x = 0;
+// Console.WriteLine("Введите целое число A: ");
+//    string A = Console.ReadLine();
+//    int x = 0;
     
-   for (int i = 0; i < A.Length; i++)
-    {
-      //Console.WriteLine(Int32.Parse(Convert.ToString(A[i])));
-      if (A.Length == 1)
-      {
-       x = Int32.Parse(Convert.ToString(A[i]));
-      }
-      else
-      {
+//    for (int i = 0; i < A.Length; i++)
+//     {
+//       //Console.WriteLine(Int32.Parse(Convert.ToString(A[i])));
+//       if (A.Length == 1)
+//       {
+//        x = Int32.Parse(Convert.ToString(A[i]));
+//       }
+//       else
+//       {
      
-      x = x + Int32.Parse(Convert.ToString(A[i]));
-      }
-    } 
-    Console.WriteLine(x);
+//       x = x + Int32.Parse(Convert.ToString(A[i]));
+//       }
+//     } 
+//     Console.WriteLine(x);
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++ )
+  {
+    arr[i] = new Random().Next(-100, 100);
+  }
+Console.WriteLine(String.Join(',', arr));
+     
